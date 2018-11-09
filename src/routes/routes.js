7066fb3f5f43250f73f9b8968f7680f47b445f9d
Walter <walter.lindop@yale.edu>
2018-11-09 22:35:14 +0000

    Adding routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,10 @@
 const Router = require('koa-router');
 const indexControllers = require('../controllers/index.js');
+const aboutControllers = require('../controllers/about.js');
 
 const router = new Router();
 router.get('/', indexControllers.index);
+
+router.get('/about', aboutControllers.index)
 
 module.exports = router;
