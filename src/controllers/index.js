4182795937b91eeea9e339dda261ee8f952b6741
Walter <walter.lindop@yale.edu>
2018-11-14 01:26:15 +0000

    Updating lists in index page

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,26 +2,18 @@
  * @param  {Context} ctx - A Koa Context
  * @returns {Promise} - Returns a promise that resolves to undefined
  */
-const bluebird = require('bluebird');
-const pgp = require('pg-promise')({ promiseLib: bluebird });
 
-db = pgp(process.env.DATABASE_URL);
+
 
 async function index(ctx) {
     
     const eventsModel = require('../models/events.js');
-    queryResult=await eventsModel.getByLocation(db,'NH');
-    console.log(queryResult); //Showing just New Haven. 
+    const events = await eventsModel.getAll(ctx.db);
     const waysOfBeingAwesome= [];
 
-    for (i in queryResult){
-        console.log(queryResult[i].title);
-        waysOfBeingAwesome.push(queryResult[i].title);
-    }
-    console.log(waysOfBeingAwesome);
     const template = 'index.njk';
  
-    return ctx.render(template, { waysOfBeingAwesome });
+    return ctx.render(template, { waysOfBeingAwesome, events });
 }
 
 module.exports = {
