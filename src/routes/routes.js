c398002102237091fda277a0f247eeb6fd0e9986
Walter <walter.lindop@yale.edu>
2018-12-02 19:04:35 +0000

    walters links

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,13 +4,9 @@ const aboutControllers = require('../controllers/about.js');
 const newEventControllers = require('../controllers/newEvent.js');
 const eventDetailsControllers = require('../controllers/eventDetails.js');
 const rsvpControllers = require('../controllers/rsvp.js');
-<<<<<<< HEAD
 const donateControllers= require ('../controllers/donate.js');
-=======
 
->>>>>>> Updating changes to SQL
 const router = new Router();
-
 router.get('/', indexControllers.index);
 router.get('/events/new', newEventControllers.newEvent);
 router.post('/events/new', newEventControllers.newEventPost);
@@ -19,9 +15,6 @@ router.get('/events', eventDetailsControllers.eventDetails);
 router.get('/events/:id', eventDetailsControllers.eventDetails);
 router.get('/about', aboutControllers.index);
 router.post('/rsvp/:id', rsvpControllers.attendeeRegistrationPost);
-<<<<<<< HEAD
 router.get('/donate', donateControllers.donate);
 
-=======
->>>>>>> Updating changes to SQL
 module.exports = router;
